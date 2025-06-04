000b867317479237fcd2d1851b48c6043873637d
Vlad50-50 <150382920+Vlad50-50@users.noreply.github.com>
2025-06-04 17:25:58 +0300

    degugging: 04.06.25

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const DEFAULT = {
         top: ((maxBottom_pos - 100) / 2) - 60
     },
     size: {
-        width: 200,
-        height: 100
+        width: 300,
+        height: 300
     },
     content: "",
     color: ""
@@ -28,17 +28,27 @@ class DWindow {
             console.warn("It is undefiened.");
         }
 
+        this.data.name = this.data.name || DEFAULT.name;
+
+        this.data.position = this.data.position || {};
+
+        this.data.size = this.data.size || {};
+        this.data.size.width = this.data.size.width || DEFAULT.size.width;
+        this.data.size.height = this.data.size.height || DEFAULT.size.height;
+
+        this.data.content = this.data.content || DEFAULT.content;
+        this.data.color = this.data.color || DEFAULT.color;
+
         this.el = document.createElement('div');
         console.log(this.data);
 
         this.el.classList.add('draggable-window');
         this.el.style.width = this.data.size.width + 'px';
         this.el.style.height = this.data.size.height + 'px';
-        this.el.style.left = this.data.position.left + 'px';
-        this.el.style.top = this.data.position.top + 'px';
+
         this.el.style.background = this.data.color;
-        this.el.style.position = 'absolute';
-        this.el.style.cursor = 'grab';
+
+
         this.el.innerHTML = `
         <div class="win-blocks">
             <div class="win-name">${this.data.name}</div>
@@ -51,13 +61,26 @@ class DWindow {
         <div class="content">${this.data.content}</div>
         `;
 
+        this.windowHeader = this.el.querySelector('.win-blocks');
+
         zIndex_count++;
         this.el.style.zIndex = zIndex_count;
         console.log("Z-index:" + zIndex_count);
 
         document.body.appendChild(this.el);
 
+        this.el.style.position = 'absolute';
+
+        if (this.data.position.left == undefined || this.data.position.left < maxLeft_pos || this.data.position.left > maxRight_pos)
+            this.el.style.left = DEFAULT.position.left + 'px';
+        else this.el.style.left = this.data.position.left + 'px';
+
+        if (this.data.position.top == undefined || this.data.position.top < maxTop_pos || this.data.position.top > maxBottom_pos)
+            this.el.style.top = DEFAULT.position.top + 'px';
+        else this.el.style.top = this.data.position.top + 'px';
+
         this.isDragging = false;
+        this.isMaximased = false;
         this.offsetX = 0;
         this.offsetY = 0;
 
@@ -67,9 +90,12 @@ class DWindow {
         this.onCloseClick = this.onCloseClick.bind(this);
         this.onHideClick = this.onHideClick.bind(this);
         this.onMaximaseClick = this.onMaximaseClick.bind(this);
+        this.onMinimizeClickBound = this.onMinimaseClick.bind(this);
+        this.onMouseClick = this.onMouseClick.bind(this);
 
+        this.windowHeader.addEventListener('mousedown', this.onMouseDown);
+        this.el.addEventListener('mousedown', this.onMouseClick);
 
-        this.el.addEventListener('mousedown', this.onMouseDown);
         document.addEventListener('mousemove', this.onMouseMove);
         document.addEventListener('mouseup', this.onMouseUp);
 
@@ -86,12 +112,15 @@ class DWindow {
     }
 
     onMouseDown(e) {
+        if (this.isMaximased) return;
         this.isDragging = true;
         const rect = this.el.getBoundingClientRect();
         this.offsetX = e.clientX - rect.left;
         this.offsetY = e.clientY - rect.top;
-        this.el.style.cursor = 'grabbing';
+        this.windowHeader.style.cursor = 'grabbing';
+    }
 
+    onMouseClick() {
         zIndex_count++;
         this.el.style.zIndex = zIndex_count;
         console.log("Z-index:" + zIndex_count);
@@ -99,6 +128,7 @@ class DWindow {
 
     onMouseMove(e) {
         if (!this.isDragging) return;
+        if (this.isMaximased) return;
 
         let newLeft = e.clientX - this.offsetX;
         let newTop = e.clientY - this.offsetY;
@@ -118,9 +148,13 @@ class DWindow {
     onMouseUp() {
         if (!this.isDragging) return;
         this.isDragging = false;
-        this.el.style.cursor = 'grab';
+        this.windowHeader.style.cursor = 'grab';
 
-        console.log(`Координати вікна: left=${this.el.style.left}, top=${this.el.style.top}`);
+        const cs = getComputedStyle(this.el);
+        this.data.position.left = parseInt(cs.left);
+        this.data.position.top = parseInt(cs.top);
+
+        console.log(`Коорди: left=${this.data.position.left}, top=${this.data.position.top}`);
     }
 
     onCloseClick(e) {
@@ -129,34 +163,49 @@ class DWindow {
     }
 
     onMaximaseClick() {
+        if (this.isMaximased) return;
+
+        this.isMaximased = true;
         console.log("Maximase clicked");
+
+        const cs = getComputedStyle(this.el);
+        this.data.position.left = parseInt(cs.left);
+        this.data.position.top = parseInt(cs.top);
 
         this.el.style.width = '100%';
         this.el.style.height = '100%';
         this.el.style.left = 0 + 'px';
         this.el.style.top = 0 + 'px';
 
+        this.windowHeader.style.cursor = 'default';
+
         this.resisebtn.removeEventListener('click', this.onMaximaseClick);
         this.resisebtn.addEventListener('click', this.onMinimaseClick.bind(this));
     }
 
     onMinimaseClick() {
+        this.isMaximased = false;
         console.log("Minimase clicked");
 
         this.el.style.width = this.data.size.width + 'px';
         this.el.style.height = this.data.size.height + 'px';
-        this.el.style.left = (maxRight_pos - this.el.offsetWidth) / 2 + 'px';
-        this.el.style.top = ((maxBottom_pos - this.el.offsetHeight) / 2) - 60 + 'px';
+        this.el.style.left = this.data.position.left + 'px';
+        this.el.style.top = this.data.position.top + 'px';
+
+        this.windowHeader.style.cursor = 'grab';
+
+        console.log(this.data.position.top);
+
 
         this.resisebtn.removeEventListener('click', this.onMaximaseClick);
         this.resisebtn.addEventListener('click', this.onMaximaseClick.bind(this));
     }
 
-    onHideClick(){
+    onHideClick() {
         this.el.style.display = 'none';
     }
 
-    onShowClick(){
+    onShowClick() {
         this.el.style.display = '';
     }
 
@@ -164,6 +213,14 @@ class DWindow {
         console.log('Destroying window:', this.el);
 
         this.el.removeEventListener('mousedown', this.onMouseDown);
+        this.el.removeEventListener('mousedown', this.onMouseClick);
+
+        this.minimize.removeEventListener('click', this.onHideClick);
+        this.resisebtn.removeEventListener('click', this.onMaximaseClick);
+        this.closeBtn.removeEventListener('click', this.onCloseClick);
+
+        this.windowHeader.removeEventListener('mousedown', this.onMouseDown);
+
         document.removeEventListener('mousemove', this.onMouseMove);
         document.removeEventListener('mouseup', this.onMouseUp);
 
@@ -173,3 +230,5 @@ class DWindow {
 }
 
 let tstWindow = new DWindow();
+let masive = [];
+masive.push(tstWindow);
